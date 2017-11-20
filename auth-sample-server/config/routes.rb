2cfcf5dca86b8cb62af20ca735a06ae16447953d
kkyouhei <hope637@gmail.com>
2017-11-20 17:08:47 +0900

    ログイン・ログアウト実装

--- auth-sample-server/config/routes.rb
+++ auth-sample-server/config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  root to: 'pages#index'
+  root :to => 'users#show'
+  resources :user_sessions
+  resources :users
+  get 'login' => 'user_sessions#new', :as => :login
+  get 'logout' => 'user_sessions#destroy', :as => :logout
 end
