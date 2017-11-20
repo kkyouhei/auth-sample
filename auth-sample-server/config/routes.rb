3626d6c2c0f4ea6285c55992f932f3fe97e4a11a
kkyouhei <hope637@gmail.com>
2017-11-21 04:48:30 +0900

    userを取得するAPIを作成

--- auth-sample-server/config/routes.rb
+++ auth-sample-server/config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  namespace :api do
+    resources :users, only: [:show]
+  end
+
   root :to => 'users#show'
   resources :user_sessions
-  resources :users
+  resources :users, only: [:show]
   get 'login' => 'user_sessions#new', :as => :login
   get 'logout' => 'user_sessions#destroy', :as => :logout
 end
