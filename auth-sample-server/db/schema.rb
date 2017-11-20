c9be5c8844b6ce45d0235d81961a9ab8e3c9989e
kkyouhei <hope637@gmail.com>
2017-11-20 15:01:06 +0900

    bin/rails generate sorcery:install --model User

--- auth-sample-server/db/schema.rb
+++ auth-sample-server/db/schema.rb
@@ -10,12 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171119181558) do
+ActiveRecord::Schema.define(version: 20171120051542) do
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", null: false
+    t.string "crypted_password"
+    t.string "salt"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
