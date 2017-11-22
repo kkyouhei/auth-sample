ee4c0da5384d6795360e70c555bfc9b4660c44f2
kkyouhei <hope637@gmail.com>
2017-11-23 05:13:34 +0900

    add auth client app

---
+++ auth-sample-client/auth-sample-clientTests/auth_sample_clientTests.swift
@@ -0,0 +1,36 @@
+//
+//  auth_sample_clientTests.swift
+//  auth-sample-clientTests
+//
+//  Created by 狩野 恭平 on 2017/11/23.
+//  Copyright © 2017年 狩野 恭平. All rights reserved.
+//
+
+import XCTest
+@testable import auth_sample_client
+
+class auth_sample_clientTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
